867f7f57c95f51a0111f039925fd5a7dc5a60951
sirshendusamanta <sirshendu96478372@gmail.com>
2025-04-29 10:26:25 +0530

    kl

--- 4_FutOiHistoty/scripts/FutOiHistory.js
+++ 4_FutOiHistoty/scripts/FutOiHistory.js
@@ -12,91 +12,121 @@ function MakeTable(data, Nifty500IndustryJson)
         
     });
 
+    ////////////////// Find Unique Industry and populate in the Industry Filter  //////////////////////////
+    let uniqueIndustryList = [];
+    Object.keys(Nifty500IndustryJson).forEach(stock => {
+        uniqueIndustryList = [...new Set([...uniqueIndustryList, Nifty500IndustryJson[stock]])];
+    });
 
-    /// Prepare the table
-    const table = document.getElementById('FutOiHistory');
-    const tbody = table.querySelector('tbody');
-    tbody.innerHTML = ''; // Clear previous rows
+    console.log(uniqueIndustryList);
 
-    uniqueStockList.forEach(stock => {
+    const IndustrylFilter = document.getElementById('IndustrylFilter');
+    IndustrylFilter.replaceChildren(); // Removes all exiting options
+    const option1 = document.createElement('option'); option1.value = '';  option1.textContent = '-- Industry --'; IndustrylFilter.appendChild(option1);
+    //let FutPosition = ["Long Buildup", "Short Covering", "Short Buildup", "Long Unwinding", "No Change"];
+    for (let i=0; i < uniqueIndustryList.length; i++)
+    {
+            const option1 = document.createElement('option');
+            option1.value = uniqueIndustryList[i]; option1.textContent = uniqueIndustryList[i]; IndustrylFilter.appendChild(option1);
+    }
 
-	 let StockIndustry = "";
-	 try {
-		 StockIndustry = Nifty500IndustryJson[stock];
-	 } catch (err) {
-		 console.log(`Industry error for : ${stock}`);
-	 }
-        ////// make the table
-        const row = document.createElement('tr');
-        let rowContent = `
-                        <td><div style="font-size:18px; font-weight: 300; background-color : #e8eaed">${stock}</div></td>
-                        <td style="width: 50px;">${StockIndustry}</td>
-                        <td>
-                            <div class="TableOiHistory">
-                        `;
 
-        Object.keys(data).forEach(date => {
-            
+    const filterIndustry = document.getElementById('IndustrylFilter');
 
-            Object.keys(data[date]).forEach(s => {
-                if (s === stock)
-                {
-                    ////////// Calculate total Div Height (Fut OI Cng)
-                    let FutOiCngList = [];
-                    Object.keys(data).forEach(DATE => { Object.keys(data[DATE]).forEach(STOCK => { if (STOCK == stock) {FutOiCngList.push(Math.abs(data[DATE][STOCK].FutOiPer));}; });  });
-                    //console.log(TotalDivHeight);
+    // Add Events Lisntener to the filter
+    filterIndustry.addEventListener('change', function () {
 
+        /// Prepare the table
+        const table = document.getElementById('FutOiHistory');
+        const tbody = table.querySelector('tbody');
+        tbody.innerHTML = ''; // Clear previous rows
+
+        const selectedValue = filterIndustry.value;
+
+        uniqueStockList.forEach(stock => {
+
+        let StockIndustry = "";
+        try {
+            StockIndustry = Nifty500IndustryJson[stock];
+        } catch (err) {
+            console.log(`Industry error for : ${stock}`);
+        }
+
+            if (selectedValue === '' || StockIndustry === selectedValue) {
+                ////// make the table
+                const row = document.createElement('tr');
+                let rowContent = `
+                                <td><div style="font-size:18px; font-weight: 300; background-color : #e8eaed">${stock}</div></td>
+                                <td style="width: 50px;">${StockIndustry}</td>
+                                <td>
+                                    <div class="TableOiHistory">
+                                `;
+
+                Object.keys(data).forEach(date => {
                     
 
-                    let colo = "red";
-                    let DivHeight = data[date][stock].FutOiPer; 
+                    Object.keys(data[date]).forEach(s => {
+                        if (s === stock)
+                        {
+                            ////////// Calculate total Div Height (Fut OI Cng)
+                            let FutOiCngList = [];
+                            Object.keys(data).forEach(DATE => { Object.keys(data[DATE]).forEach(STOCK => { if (STOCK == stock) {FutOiCngList.push(Math.abs(data[DATE][STOCK].FutOiPer));}; });  });
+                            //console.log(TotalDivHeight);
+
+                            
+
+                            let colo = "red";
+                            let DivHeight = data[date][stock].FutOiPer; 
+                            
+                            //if (DivHeight < 0) { colo = "red";  } else { colo = "green"; }
+
+                            let AbsDivHeight = (Math.abs(DivHeight)/Math.max(...FutOiCngList))*50; // Maximum width would be 50 px
+                            //let translateY = DivHeight < 0 ? `0px` : `-${AbsDivHeight}px`; // Move green bars up
+                            
+                            let positionType = data[date][stock].Fut_category;
+                            let pricecng = data[date][stock].PriceCng;
+
+                            if (positionType ==="Long Buildup" || positionType === "Short Covering")
+                            {
+                                colo = "green";
+                            }
+                            else
+                            {
+                                colo = "red";
+                            }
+                            let translateY = colo === "green" ? -AbsDivHeight/2 : AbsDivHeight/2;
+
+                            rowContent += `
+                                        <div class="TableOiHistoryDiv" 
+                                            style="
+                                                background-color: ${colo}; 
+                                                height: ${AbsDivHeight}px;
+                                                bottom: 50%; 
+                                                transform: translateY(${translateY}px);
+                                            "
+                                            onmouseover="showTooltip(event, '${date}', '${pricecng.toFixed(2)}%', '${positionType}', '${DivHeight.toFixed(2)}%')"
+                                            onmouseout="hideTooltip()
+                                            ">
+                                        </div>
+                            `;
+                        }
+                    });
+        
                     
-                    //if (DivHeight < 0) { colo = "red";  } else { colo = "green"; }
 
-                    let AbsDivHeight = (Math.abs(DivHeight)/Math.max(...FutOiCngList))*50; // Maximum width would be 50 px
-                    //let translateY = DivHeight < 0 ? `0px` : `-${AbsDivHeight}px`; // Move green bars up
-                    
-                    let positionType = data[date][stock].Fut_category;
-                    let pricecng = data[date][stock].PriceCng;
+                });
 
-                    if (positionType ==="Long Buildup" || positionType === "Short Covering")
-                    {
-                        colo = "green";
-                    }
-                    else
-                    {
-                        colo = "red";
-                    }
-                    let translateY = colo === "green" ? -AbsDivHeight/2 : AbsDivHeight/2;
+                rowContent += `</div>
+                                </td>`;
 
-                    rowContent += `
-                                <div class="TableOiHistoryDiv" 
-                                    style="
-                                        background-color: ${colo}; 
-                                        height: ${AbsDivHeight}px;
-                                        bottom: 50%; 
-                                        transform: translateY(${translateY}px);
-                                    "
-                                    onmouseover="showTooltip(event, '${date}', '${pricecng.toFixed(2)}%', '${positionType}', '${DivHeight.toFixed(2)}%')"
-                                    onmouseout="hideTooltip()
-                                    ">
-                                </div>
-                    `;
-                }
-            });
- 
-            
+                row.innerHTML = rowContent;
+                tbody.appendChild(row);
+            }
 
         });
-
-        rowContent += `</div>
-                        </td>`;
-
-        row.innerHTML = rowContent;
-        tbody.appendChild(row);
-
-    });
     table.style.display = 'table';
+    });
+    
     
 }
 
